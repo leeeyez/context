ef230f1fca5fdc58efd8e663641d531916627ffb
solux <lee.yeji0327@gmail.com>
2024-08-27 23:53:46 +0900

    [feat] theme style 관련 파일

---
+++ src/theme/theme.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  bgColor: "white",
+  textColor: "black",
+  borderColor: "1px solid black",
+};
+
+export const darkTheme = {
+  bgColor: "black",
+  textColor: "white",
+  borderColor: "1px solid white",
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
